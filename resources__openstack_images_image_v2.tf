2f555e5b73853c5214018120e878ec7f0a98c3cf
tannevaled <tannevaled@users.noreply.github.com>
2024-10-15 15:16:01 +0200

    troubleshoot "Error: Provider produced inconsistent final plan"

--- resources__openstack_images_image_v2.tf
+++ resources__openstack_images_image_v2.tf
@@ -7,11 +7,11 @@ resource openstack_images_image_v2 image {
     disk_format = local.image_format
 
     properties = {
-        "DEPLOYMENT_UUID": local.deployment.uuid,
-        "DEPLOYMENT_ID": local.deployment.id,
-        "architecture": each.key,
+    //    "DEPLOYMENT_UUID": local.deployment.uuid,
+    //    "DEPLOYMENT_ID": local.deployment.id,
+    //    "architecture": each.key,
     //    "os_distro": local.image_distribution,
     //    "os_version": local.image_version,
-        "img_config_drive": local.config_drive
+    //    "img_config_drive": local.config_drive
     }
 }

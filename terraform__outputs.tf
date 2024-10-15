a7af9b30b1a026e0c1cdb2f1c5bb94df0e2b72a5
tannevaled <tannevaled@users.noreply.github.com>
2024-10-15 16:52:03 +0200

    wip

--- terraform__outputs.tf
+++ terraform__outputs.tf
@@ -1,6 +1,6 @@
 output outputs {
     description = "the deployed images"
-    value = element(openstack_images_image_v2.image.*, 0)
+    value = jsonencode(element(openstack_images_image_v2.image.*, 0))
 }
 
 output deployment {

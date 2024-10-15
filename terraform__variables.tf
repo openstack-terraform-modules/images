f27f808a812b16a167cde2138a73338b949bbf6e
tannevaled <tannevaled@users.noreply.github.com>
2024-10-15 10:14:38 +0200

    wip

--- terraform__variables.tf
+++ terraform__variables.tf
@@ -8,16 +8,16 @@ variable deployment {
 variable images {
     description = "A multi-arch image to upload"
     type = object({
-        "name" = string
-        "images" = object({
-          "distribution" = string
-          "version" = string
-          "urls" = object({
-            "amd64" = string
-            "arm64" = string
-            "riscv64" = string
-            "ppc64el" = string
-            "s390x" = string
+        name = string
+        images = object({
+          distribution = string
+          version = string
+          urls = object({
+            amd64 = string
+            arm64 = string
+            riscv64 = string
+            ppc64el = string
+            s390x = string
           })
         })
     })
